ef0e4debe2f6f6b587b8940898c29980bff40117
Rafiu Moshood <meezyloaded@gmail.com>
2022-07-25 20:38:55 +0100

    chore: add static files

--- public/index.php
+++ public/index.php
@@ -7,4 +7,9 @@ use App\App;
 require_once __DIR__ . '/../vendor/autoload.php';
 $router = require_once __DIR__ . '/../routes.php';
 
+$path = __DIR__ . $_SERVER['REQUEST_URI'];
+if (file_exists($path)) {
+    return false;
+}
+
 (new App($router))->run();
