905863ed8ac522e7f7c1833f3313ad8df406b669
Rafiu Moshood <meezyloaded@gmail.com>
2022-08-05 11:48:57 +0100

    feat: add models

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,16 +1,6 @@
 <?php
 
 namespace App\Models;
-
-use App\App;
-use App\Interfaces\DatabaseInterface;
-
 abstract class Model
 {
-    protected \PDO $db;
-
-    public function __construct(DatabaseInterface $database)
-    {
-        $this->db = $database->getInstance();
-    }
 }
